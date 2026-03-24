64408d3a6ea8a834920c6462e7e8f45eaa13ff10
Manabendu-ai <rikugithub@gmail.com>
2026-03-25 03:32:24 +0530

    Added Swagger UI implementation

--- quiz-service/src/main/java/com/riku/quiz_service/feign/QuizInterface.java
+++ quiz-service/src/main/java/com/riku/quiz_service/feign/QuizInterface.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @FeignClient("QUESTION-SERVICE")
-
 public interface QuizInterface {
     @GetMapping("/question/allQuestions")
     ResponseEntity<?> getAllQuestions();
